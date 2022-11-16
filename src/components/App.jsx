8f1771059270fd9d398a4ebf6e7865c4aa3e52bb
Kvasov Dmytro <dmytro.kvasov@vilmate.com>
2022-11-16 14:48:27 +0200

    Added skeleton of src structure

---
+++ src/components/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from '../styles/App.module.css';
+
+const App = () =>
+    <div className={styles.container}>
+    <p className={styles.text}>Hello React</p>
+</div>
+
+export default App;
